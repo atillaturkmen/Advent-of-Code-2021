6d91b0f68b6007789c0f9880e7e36375112edbf8
atillaturkmen <turkmenatilla522@gmail.com>
2021-12-03 10:58:23 +0300

    day 3 part 1

---
+++ day-03/part1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    string cur;
+    cin >> cur;
+    ll length = cur.length();
+    ll sum[length] = {0};
+    do {
+        for (int i = 0; i < length; i++) {
+            if (cur[i] == '1') {
+                sum[i]++;
+            } else {
+                sum[i]--;
+            }
+        }
+    } while (cin >> cur);
+    ll gamma = 0;
+    ll epsilon = 0;
+    for (ll i = 0; i < length; i++) {
+        if (sum[i] > 0) {
+            gamma += pow(2, length - i - 1);
+        } else {
+            epsilon += pow(2, length - i - 1);
+        }
+    }
+    cout << gamma * epsilon;
+    return 0;
+}
